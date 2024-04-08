dcb056ee1ab4ab4cb13a431bd6ef9a742dce10b6
weihao <podoid@outlook.com>
2024-04-08 14:10:45 +0800

    xxxx

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require "vendor/autoload.php";
+
+$a = new Payment\test();
+
+$a->alipay();
---
+++ src/payment/test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Payment;
+
+class test
+{
+    public function alipay()
+    {
+        echo 'alipay v1.0.0';
+    }
+}
